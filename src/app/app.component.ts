7ef4648476428573e415c40474a24957683bbff4
Brock Tubre <brock.tubre@csra.com>
2017-06-07 09:54:25 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Account, Stormpath } from 'angular-stormpath';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  user$: Observable<Account | boolean>;
+ 
+  constructor(private stormpath: Stormpath) {
+    this.user$ = this.stormpath.user$;
+  }
+ 
+  logout(): void {
+    this.stormpath.logout();
+  }
 }
